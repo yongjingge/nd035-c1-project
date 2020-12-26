2ea43f70d788c984029629d2a840d5a6a91e1771
yongjing <near711@outlook.com>
2020-12-26 17:47:27 +0800

    custom error page and controller added

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/ErrorController.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Run custom logic when errors occur (replace the default error controller)
+ */
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+    @Override
+    public String getErrorPath () {
+        return "error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError (HttpServletRequest request) {
+        // get error status
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "500";
+            }
+        }
+
+        return "error";
+    }
+}
