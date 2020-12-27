88847798aabae70f42b47828dedf1d7f678ab744
yongjing <near711@outlook.com>
2020-12-27 20:54:55 +0800

    MaxUploadSizeException can not be caught, needs fix

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/GlobalExceptionHandler.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/exception/GlobalExceptionHandler.java
@@ -5,15 +5,22 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+//    @ExceptionHandler(MaxUploadSizeExceededException.class)
+//    public String handleMaxSizeException (MaxUploadSizeExceededException e, Model model) {
+//        model.addAttribute("errorhappens", true);
+//        model.addAttribute("errormsg", "File size exceeds limit!");
+//        return "result";
+//    }
+
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleMaxSizeException (MaxUploadSizeExceededException e, Model model) {
-        model.addAttribute("errorhappens", true);
-        model.addAttribute("errormsg", "File size exceeds limit!");
-        return "result";
+    public String handleMaxSizeException (MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errormsg", "File size exceeds limit!");
+        return "redirect:/result";
     }
 
     @ExceptionHandler(MultipartException.class)
